fad1e9da564aafbf681db447cd93533d962504f5
ktoulliou <kilian.meze@gmail.com>
2020-06-11 19:31:53 +0200

    ajout des dossiers a venir

--- infra.nix
+++ infra.nix
@@ -4,9 +4,19 @@
     imports = [ ./web/configuration.nix
               ];
   };
+  nas = {
+    deployment.targetHost = "nixos-nas";
+    imports = [ ./nas/configuration.nix
+              ];
+  };
   pc1 = {
     deployment.targetHost = "nixos-pc1";
     imports = [ ./pc1/configuration.nix
+              ];
+  };
+  pc2 = {
+    deployment.targetHost = "nixos-pc2";
+    imports = [ ./pc2/configuration.nix
               ];
   };
   network.description = "my network";
